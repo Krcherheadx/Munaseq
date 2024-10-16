551299be579723fa14447318d43ab61c42874140
Hisham Alsuhaibani <144757552+Krcherheadx@users.noreply.github.com>
2024-10-16 18:18:04 +0300

    fix minor bug

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3002",
-        pathname: "/images/**",
-      },
-    ],
+    domains: ["munaseq.s3.eu-north-1.amazonaws.com"],
   },
 };
 
